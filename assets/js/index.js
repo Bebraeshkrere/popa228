4e062feca028a7d97e68349ae6d761f57ea44120
Bebraeshkrere <144522850+Bebraeshkrere@users.noreply.github.com>
2023-12-09 18:28:17 +1000

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+let name = 'berbra'
+
+console.log(name);
+var name1 = "dfdff"
+console.log(name1)
+
+
+function name(params) {
+    let LetName = 'let'
+    var VarName ='Var'
+    console.log(LetName, VarName)
+
+    if (1==1)
+}
